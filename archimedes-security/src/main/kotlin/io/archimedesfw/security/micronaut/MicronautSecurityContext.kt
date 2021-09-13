8b16380d29e5db0af2663979735c68f8e462c9c6
Alejandro Pérez García <alejandropg@autentia.com>
2021-09-13 09:03:16 +0200

    In Micronaut 3.0.0 you cannot configure `rolesName` any more

--- archimedes-security/src/main/kotlin/io/archimedesfw/security/micronaut/MicronautSecurityContext.kt
+++ archimedes-security/src/main/kotlin/io/archimedesfw/security/micronaut/MicronautSecurityContext.kt
@@ -4,20 +4,11 @@ import io.archimedesfw.security.Security
 import io.archimedesfw.security.SecurityContext
 import io.archimedesfw.security.ThreadLocalSecurityContextHolder
 import io.micronaut.context.annotation.Context
-import io.micronaut.context.annotation.Value
-import io.micronaut.security.token.config.TokenConfiguration
-import io.micronaut.security.token.config.TokenConfigurationProperties
 import io.micronaut.security.utils.SecurityService
 
 @Context
 class MicronautSecurityContext(
     private val securityService: SecurityService,
-
-    // We use the same rolesNames that is configured in the `application.yml`.
-    // If the property `roles-name` is not set we use the same default value than Micronaut.
-    @Value("\${${TokenConfigurationProperties.PREFIX}.roles-name:${TokenConfiguration.DEFAULT_ROLES_NAME}}")
-    private val rolesName: String
-
 ) : SecurityContext {
 
     init {
@@ -29,6 +20,6 @@ class MicronautSecurityContext(
     override val username: String
         get() = securityService.username().orElseThrow { IllegalStateException("Check failed.") }
 
-    override fun has(permission: String): Boolean = securityService.hasRole(permission, rolesName)
+    override fun has(permission: String): Boolean = securityService.hasRole(permission)
 
 }
