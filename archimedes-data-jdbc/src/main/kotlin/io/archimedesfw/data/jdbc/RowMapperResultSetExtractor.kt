65159d99a3b63a8cfc3f91fccb61c0a1b7c9970a
Alejandro Pérez García <alejandropg@autentia.com>
2021-12-12 09:23:07 +0100

    Improve Kotlin syntax and set lambda as last parameter
    Example: `val rm = RowMapperResultSetExtractor { ... }`

--- archimedes-data-jdbc/src/main/kotlin/io/archimedesfw/data/jdbc/RowMapperResultSetExtractor.kt
+++ archimedes-data-jdbc/src/main/kotlin/io/archimedesfw/data/jdbc/RowMapperResultSetExtractor.kt
@@ -1,12 +1,13 @@
 package io.archimedesfw.data.jdbc
 
 import java.sql.ResultSet
-import java.util.ArrayList
 
 class RowMapperResultSetExtractor<T>(
-    private val rowMapper: RowMapper<T>,
-    private val rowsExpected: Int = 0
+    private val rowsExpected: Int,
+    private val rowMapper: RowMapper<T>
 ) : ResultSetExtractor<List<T>> {
+
+    constructor(rowMapper: RowMapper<T>) : this(0, rowMapper)
 
     override fun extractData(rs: ResultSet): List<T> {
         val results = if (rowsExpected > 0) ArrayList<T>(rowsExpected) else ArrayList<T>()
