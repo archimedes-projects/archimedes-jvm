c353e922499775f94f4a3dc57d0ac06fb733bd67
Alejandro Pérez García <483541+alejandropg@users.noreply.github.com>
2024-07-12 11:55:55 +0200

    feat: add hashCode() function to ArrayParameter

--- archimedes-data-jdbc/src/main/kotlin/io/archimedesfw/data/sql/criteria/parameter/ArrayParameter.kt
+++ archimedes-data-jdbc/src/main/kotlin/io/archimedesfw/data/sql/criteria/parameter/ArrayParameter.kt
@@ -20,13 +20,15 @@ class ArrayParameter<T>(
 
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as ArrayParameter<*>
+        if (other !is ArrayParameter<*>) return false
 
         if (parameters != other.parameters) return false
 
         return true
+    }
+
+    override fun hashCode(): Int {
+        return parameters.hashCode()
     }
 
 }
