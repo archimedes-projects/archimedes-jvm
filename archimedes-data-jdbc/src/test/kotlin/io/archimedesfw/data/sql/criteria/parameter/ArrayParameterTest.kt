f2430f3b50bc710ee7fab6e488b36fa1a7b57e73
Alejandro Pérez García <483541+alejandropg@users.noreply.github.com>
2024-07-12 11:55:55 +0200

    feat: add hashCode() function to ArrayParameter

--- archimedes-data-jdbc/src/test/kotlin/io/archimedesfw/data/sql/criteria/parameter/ArrayParameterTest.kt
+++ archimedes-data-jdbc/src/test/kotlin/io/archimedesfw/data/sql/criteria/parameter/ArrayParameterTest.kt
@@ -2,21 +2,30 @@ package io.archimedesfw.data.sql.criteria.parameter
 
 import io.archimedesfw.data.sql.criteria.Expressions
 import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotEquals
 import org.junit.jupiter.api.Test
 
-class ArrayParameterTest {
+internal class ArrayParameterTest {
 
     @Test
     fun `equals true`() {
-        val arr1 = Expressions.parameter(listOf<Int>(1,2,3))
-        val arr2 = Expressions.parameter(listOf<Int>(1,2,3))
-        Assertions.assertEquals(arr1, arr2)
+        val arr1 = Expressions.parameter(listOf<Int>(1, 2, 3))
+        val arr2 = Expressions.parameter(listOf<Int>(1, 2, 3))
+        assertEquals(arr1, arr2)
     }
 
     @Test
     fun `equals false`() {
-        val arr1 = Expressions.parameter(listOf<Int>(1,2,3))
-        val arr2 = Expressions.parameter(listOf<Int>(11,22,33))
-        Assertions.assertNotEquals(arr1, arr2)
+        val arr1 = Expressions.parameter(listOf<Int>(1, 2, 3))
+        val arr2 = Expressions.parameter(listOf<Int>(11, 22, 33))
+        assertNotEquals(arr1, arr2)
+    }
+
+    @Test
+    fun `hash code`() {
+        val arr1 = Expressions.parameter(listOf<Int>(1, 2, 3))
+        val arr2 = Expressions.parameter(listOf<Int>(1, 2, 3))
+        assertEquals(arr1.hashCode(), arr2.hashCode())
     }
 }
